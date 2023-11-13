bb4c307f9d7a4b6b769e3c3025630caeb37f66d4
jonathan_baraza <jbaraza46@gmail.com>
2023-11-13 10:07:28 +0300

    calculator viewbox

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -5,19 +5,22 @@ const ThemeToggle = () => {
   const currentTheme = useAppSelector((state) => state.theme);
   const dispatch = useDispatch();
   return (
-    <div className="w-full flex justify-between items-end text-white py-4">
+    <div
+      className="w-full flex justify-between items-end py-4"
+      style={{ color: currentTheme.theme.textColor }}
+    >
       <div className="font-bold text-xl">calc</div>
       <div className="flex space-x-3 items-end">
-        <div className="text-[10px] pb-[2px]">THEME</div>
+        <div className="text-[10px] pb-[2px] font-semibold ">THEME</div>
         <div className="flex flex-col ">
-          <div className="w-full space-x-3 flex justify-between text-white text-[10px] mb-[2px] px-2 ">
+          <div className="w-full space-x-3 flex justify-between font-semibold text-[10px] mb-[2px] px-2 ">
             <div>1</div>
             <div>2</div>
             <div>3</div>
           </div>
           <div
             className="w-full rounded-xl h-[20px] px-1 flex items-center justify-between"
-            style={{ backgroundColor: currentTheme.theme.background2 }}
+            style={{ backgroundColor: currentTheme.theme.background3 }}
           >
             <div
               className={`h-[13px]  ${
--- components/ViewBox.tsx
+++ components/ViewBox.tsx
@@ -1,11 +1,14 @@
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
 const ViewBox = () => {
-  const currentTheme = useAppSelector((state) => state.theme);
+  const { background, background2, background3, textColor, textColor2 } =
+    useAppSelector((state) => state.theme.theme);
   return (
     <div
-      style={{ backgroundColor: currentTheme.theme.background2 }}
-      className="w-full h-[12vh] rounded-lg my-3 text-white text-5xl flex flex-wrap items-end justify-end"
-    ></div>
+      style={{ backgroundColor: background2, color: textColor }}
+      className="w-full h-[12vh] rounded-lg my-3 p-2  text-4xl font-bold flex flex-wrap items-center justify-end"
+    >
+      399,230
+    </div>
   );
 };
 
