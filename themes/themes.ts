1b0c525cfff6cc897aaace75731dd2013c33d9f1
jonathan_baraza <jbaraza46@gmail.com>
2023-11-13 09:53:31 +0300

    themes init

---
+++ themes/themes.ts
@@ -0,0 +1,31 @@
+const allThemes = {
+  one: {
+    textColor: "white",
+    textColor2: "#3d4452",
+    background: "#3b4664",
+    background2: "#181f32",
+    background3: "#252d44",
+    btnClear: "#647299",
+    btnEqual: "#d13f30",
+  },
+  two: {
+    textColor: "#38382f",
+    textColor2: "#393930",
+    background: "#e6e6e6",
+    background2: "#eeeeee",
+    background3: "#d3cdce",
+    btnClear: "#388187",
+    btnEqual: "#c85401",
+  },
+  three: {
+    textColor: "#38382f",
+    textColor2: "#393930",
+    background: "#e6e6e6",
+    background2: "#eeeeee",
+    background3: "#d3cdce",
+    btnClear: "#388187",
+    btnEqual: "#c85401",
+  },
+};
+
+export default allThemes;
